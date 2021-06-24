6b3b28f3ca550adc9fc484326e5b948a46f64092
Sargaroth2511 <84723008+Sargaroth2511@users.noreply.github.com>
2021-06-24 16:08:22 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// class ElectricCar {
+//     constructor(name, url, vMax, power, acc, reach, weight, price){
+//     this.name = name;
+//     this.url = url;
+//     this.vMax = vMax;
+//     this.power = power;
+//     this.acc = acc;
+//     this.reach = reach;
+//     this.weight = weight;
+//     this.price = price;
+
+    
+//     }
+// }
+
+
+
+// const a1 = new ElectricCar('Audi e-tron 55 quattro S', 'url', 200, 408, 5.7, 436, 2595, 84450);
+// const a2 = new ElectricCar('BMW i3','url', 150, 125, 7.2, 330, 1355, 39000);
+
+
+let electricCars = JSON.parse(data);
+
+console.log(electricCars);
+
+// console.log(JSON.stringify(electricCars));
+
+// // console.log(a1, a2);
+
+// console.log(electricCars);
+// console.log(electricCars[1].name);
