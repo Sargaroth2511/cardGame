2ced2cbb970c22936132ca40c1895d0ca3a253af
Sargaroth2511 <84723008+Sargaroth2511@users.noreply.github.com>
2021-07-09 11:18:51 +0200

    Menu added

--- indexListeners.js
+++ indexListeners.js
@@ -1,18 +1,30 @@
 // Index Selectors
-const selDecksBackbtn = document.querySelector('#choosedeck .backbtn');
+const selDecksBackbtn = document.querySelector('#choosedeck #choose-deck-backbtn');
 const chooseDeckPopout = document.querySelector ('#choose-deck-popout');
 const startForm = document.querySelector ('#startform');
 const deckForm = document.querySelector ('#deck-form');
 let chosenDeck = '';
+
+let lastGame = {
+    deck: 'luxCarGame',
+    difficulty : 'easy',
+    online : false
+
+}
 
 selDecksBackbtn.addEventListener ('click', e => {
     e.preventDefault();
     chooseDeckPopout.style.display = 'none';
 });
 
+
+
 startForm.addEventListener ('click', e => {
    if(e.target.defaultValue === 'Neues Spiel') {
        chooseDeckPopout.style.display = 'grid'
+   }
+   if(e.target.defaultValue ==='Schnelles Spiel') {
+       window.location.replace(`${lastGame.deck}.html`)
    }
 });
 
